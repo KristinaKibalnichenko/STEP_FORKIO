e4a27281354a4c2200d762a66b77b5c872d05d0a
Viktoriia Tereshchenko <viktoria.viptour@gmail.com>
2020-12-11 10:18:05 +0200

    correct editor

---
+++ js/header.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector(".nav-burger"),
+  menu = document.querySelector(".nav-menu"),
+  menuItems = document.querySelectorAll(".nav-menu__item");
+
+btn.addEventListener("click", function (e) {
+  btn.classList.toggle("nav-burger--active");
+  menu.classList.toggle("nav-menu--active");
+  menuItems.classList.toggle("nav-menu--active");
+});
