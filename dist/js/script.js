d9023c9aeb356d2c3811baee230ee04695ba87ea
Kristina Kibalnichenko <nova.ua@ukr.net>
2020-12-13 16:32:31 +0200

    updated burger-menu on click outside

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,25 @@
 let btn = document.querySelector(".nav-burger"),
   menu = document.querySelector(".nav-menu"),
-  menuItems = document.querySelectorAll(".nav-menu__item");
+  menuItems = document.querySelector(".nav-menu__item");
 
-btn.addEventListener("click", function (e) {
+const toggleMenu = function() {
   btn.classList.toggle("nav-burger--active");
   menu.classList.toggle("nav-menu--active");
   menuItems.classList.toggle("nav-menu--active");
+}
+
+btn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleMenu();
 });
 
+document.addEventListener('click', function(e) {
+  const target = e.target;
+  const its_menu = target == menu || menu.contains(target);
+  const its_btnMenu = target == btn;
+  const menu_is_active = menu.classList.contains("nav-menu--active");
+  
+  if (!its_menu && !its_btnMenu && menu_is_active) {
+      toggleMenu();
+  }
+});
